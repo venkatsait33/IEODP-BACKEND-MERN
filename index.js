59731eff6b34e66c7464828bc38f0e6e6911268e
t.VenkatSai <129149218+venkatsait33@users.noreply.github.com>
2026-01-23 10:00:54 +0530

    fetch

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express, { urlencoded } from "express";
 import bodyParser from "body-parser";
-import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./src/db/db.js";
 import userRoutes from "./src/routes/user.routes.js";
@@ -10,15 +9,17 @@ import ticketRoutes from "./src/routes/ticket.routes.js";
 import auditRoutes from "./src/routes/audit.routes.js";
 import adminRoutes from "./src/routes/admin.routes.js";
 import dashboardData from "./src/routes/dashboard.routes.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use(cors());
 
 app.use(errorHandler);
 app.use("/api/v1/auth", userRoutes);
@@ -26,14 +27,6 @@ app.use("/api/v1/ticket", ticketRoutes);
 app.use("/api/v1/audit", auditRoutes);
 app.use("/api/v1/admin", adminRoutes);
 app.use("/api/v1/dashboard", dashboardData);
-
-const corsOptions = {
-  origin: "http://localhost:5173" || "https://ieodp-frontend-react.vercel.app/",
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization", "Origin", "Accept"],
-};
-app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
